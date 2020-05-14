4edd3a67a527e3a1d24dd8bf08855dd2bc10da92
Anton Vaitsehovskiy <antonvaits@gmail.com>
2020-05-14 17:15:27 +0300

    figs bugs after review

--- routes/users.js
+++ routes/users.js
@@ -7,9 +7,9 @@ routerUsers.get('/', (req, res) => {
 
 const doesUserExist = (req, res) => {
   // eslint-disable-next-line arrow-parens
-  const foundUsers = users.find(i => i._id === req.params.id);
-  if (foundUsers) {
-    res.status(200).send(foundUsers);
+  const foundUser = users.find(userItem => userItem._id === req.params.id);
+  if (foundUser) {
+    res.status(200).send(foundUser);
     return;
   }
   res.status(404).send({ message: 'Нет пользователя с таким id' });
